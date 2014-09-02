c71d40062be5a13e8fd33662090391a492a70031
Peter Schneider <peter.schneider.127@googlemail.com>
2014-09-02 20:25:46 +0200

    remove debug code

--- src/main.c
+++ src/main.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include "debug.h"
 
-static int do_fail() {
-    return E_ERROR;
-}
-
-static int do_win() {
-    return E_OK;
-}
 
 int main(int argc, char** argv) {
     printf("Hello, World\n");
-    _E(do_fail());
-    _E(do_win());
-
-    TR_CRIT("crit");
-    TR_ERR("err");
-    TR_INFO("info");
-    TR_DEBUG("debug");
-    TR_SPAM("spam");
-    
     return 0;
 }
 
