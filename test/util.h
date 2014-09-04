2d47e67823d905324b9eebb558c71413854d7d4f
Peter Schneider <peter.schneider.127@googlemail.com>
2014-09-05 00:45:25 +0200

    dd some utility macros for unit testing

---
+++ test/util.h
@@ -0,0 +1,9 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define IS_OK(x) ck_assert_int_ge(x, 0)
+#define IS_TRUE(x) ck_assert(x)
+#define IS_INT_EQ(x,y) ck_assert_int_eq(x,y)
+#define IS_P_EQ(x,y) ck_assert_ptr_eq(x,y)
+#endif
+
